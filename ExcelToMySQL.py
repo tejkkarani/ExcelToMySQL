1723b56214e2e496c8ace0a003c256a842bc0ac2
tejkkarani <tejkkarani@gmail.com>
2018-07-03 18:03:10 +0530

    usng pretty print

--- ExcelToMySQL.py
+++ ExcelToMySQL.py
@@ -1,4 +1,5 @@
 from xlrd import open_workbook
+from pprint import pprint
 wb = open_workbook('test.xlsx')
 values = []
 for s in wb.sheets():
@@ -6,9 +7,11 @@ for s in wb.sheets():
         col_names = s.row(0)
         col_value = {}
         for name, col in zip(col_names, range(s.ncols)):
-            value  = (s.cell(row,col).value)
-            try : value = str(int(value))
-            except : pass
+            value = s.cell(row,col).value
+            try:
+                value = str(int(value))
+            except :
+                pass
             col_value[name.value]=value
         values.append(col_value)
-print(values)
+pprint(values)
