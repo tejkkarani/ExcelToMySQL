86460955bcf97eb0d804bc2f588a899aaeb98ab6
tejkkarani <tejkkarani@gmail.com>
2018-07-03 18:01:18 +0530

    getting data from excel and putting it in list of dictionaries

--- ExcelToMySQL.py
+++ ExcelToMySQL.py
@@ -1,2 +1,14 @@
 from xlrd import open_workbook
 wb = open_workbook('test.xlsx')
+values = []
+for s in wb.sheets():
+    for row in range(1, s.nrows):
+        col_names = s.row(0)
+        col_value = {}
+        for name, col in zip(col_names, range(s.ncols)):
+            value  = (s.cell(row,col).value)
+            try : value = str(int(value))
+            except : pass
+            col_value[name.value]=value
+        values.append(col_value)
+print(values)
